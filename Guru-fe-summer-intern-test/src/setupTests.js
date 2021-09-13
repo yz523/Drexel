7bfbb8909975daf687b10bbf8fbb65a2679a9f7f
yz523 <yz523@drexel.edu>
2021-09-13 15:47:01 +0800

    update

--- Guru-fe-summer-intern-test/src/setupTests.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable import/no-extraneous-dependencies */
-/* We want these to be in our devDependencies because they are a part of the jest tests only */
-
-import { configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-
-configure({ adapter: new Adapter() });
